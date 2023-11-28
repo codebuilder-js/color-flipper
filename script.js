bc33c3f953fd63f38ca6a8ebe34bdb39ebbc1e98
Rafael <codebuilder.js@gmail.com>
2023-11-27 22:03:22 -0300

    Add the function that generate random rgb and add an event to the button

--- script.js
+++ script.js
@@ -4,4 +4,25 @@
     ONCLICK altera a cor de fundo utilizando o valores estáticos da variável 
     Criar função que gera números aleatórios de 0 a 255
     ONCLICK altera a cor de fundo utilizando números aleatórios no RGB
+
+    Fazer com hex e nomes nativos de cores
 */
+const btnFlipper = document.querySelector('.btn-flipper');
+const colorPlaceholder = document.querySelector('#color-placeholder');
+const rgbName = document.querySelector('#rgb-name');
+
+btnFlipper.addEventListener('click', changeBackground);
+
+function changeBackground() {
+    const red = randomRGB();
+    const green = randomRGB();
+    const blue = randomRGB();
+    const rgb = `rgb(${red}, ${green}, ${blue})`;
+
+    document.body.style.background = rgb;
+    rgbName.textContent = rgb;
+}
+
+function randomRGB() {
+    return Math.floor(Math.random() * 255);
+}
