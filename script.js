adccf494832705181acca9d282ba88727d9c870b
Rafael <codebuilder.js@gmail.com>
2023-11-27 21:34:56 -0300

    Create the main style of the page and the algorithm on script.file

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+    Armazenar referência da cor de fundo alterada
+    Armazenar referência do botão
+    ONCLICK altera a cor de fundo utilizando o valores estáticos da variável 
+    Criar função que gera números aleatórios de 0 a 255
+    ONCLICK altera a cor de fundo utilizando números aleatórios no RGB
+*/
